846cc7ade7b721a54b2bf6e61dc0fc639496b5e0
Elton Dornelas <eltondornelas@gmail.com>
2021-05-13 20:00:41 -0300

    displaying friendly error messages with anyhow crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use structopt::StructOpt;
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
@@ -12,11 +13,17 @@ fn find_default_journal_file() -> Option<PathBuf> {
     // we'll rely on a third-party crate called home to determine the directory.
     home::home_dir().map(|mut path| {
         path.push(".rusty-journal.json");
+
         path
     })
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
+    // anyhow is an excellent crate for displaying useful and pretty errors to users
+    // Because most error types can be converted to anyhow::Error, we can use ? syntax to remove
+    // the expect calls from our code. Also, note that we're using the anyhow! macro to
+    // produce an anyhow::Error on the fly that contains the provided error message.
+
     // Get the command-line arguments.
     let CommandLineArgs {
         action,
@@ -26,15 +33,15 @@ fn main() {
     // Unpack the journal file.
     let journal_file = journal_file
         .or_else(find_default_journal_file)
-        .expect("Failed to find journal file.");
+        .ok_or(anyhow!("Failed to find journal file."))?;
 
     // Perform the action.
     match action {
         Add { text } => tasks::add_task(journal_file, Task::new(text)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
+    }?;
 
-    }.expect("Failed to perform action")
-
+    Ok(())
     // println!("{:#?}", cli::CommandLineArgs::from_args());
 }
