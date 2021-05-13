3d2bf7dfa43e4e9e3098bef84edf1c8281f29f14
Elton Dornelas <eltondornelas@gmail.com>
2021-05-13 19:48:22 -0300

    using a default journal file with home crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use structopt::StructOpt;
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
+use std::path::PathBuf;
 
 mod cli;
 mod tasks;
+
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    // Because home directories vary depending on the user's operating system,
+    // we'll rely on a third-party crate called home to determine the directory.
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
 
 fn main() {
     // Get the command-line arguments.
@@ -13,7 +24,9 @@ fn main() {
     } = CommandLineArgs::from_args();
 
     // Unpack the journal file.
-    let journal_file = journal_file.expect("Failed to find journal file");
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .expect("Failed to find journal file.");
 
     // Perform the action.
     match action {
